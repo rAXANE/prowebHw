2e7a542cf3fa5050ee98b6db261135434af749c1
rAXANE <justlowena@gmail.com>
2023-12-26 14:05:54 +0500

    first

--- script.js
+++ script.js
@@ -1,96 +1,17 @@
+let height = 5; 
+let width = 10; 
 
-// // Операторы сравнения 
+let rectangle = '';
 
-// // == - оператор проверяющий на равенство 
-
-// // !!! ЛЮБОЙ ОПЕРАТОР СРАВНЕНИЯ ОТДАЕТ БУЛЕВЫЕ ЗНАЧЕНИЯ 
-
-// let x = 5 == 5; // true 
-
-// console.log(x);
-
-// // === - оператор строгой проверки
-
-// // != -  оператор проверяющий на неравенство 
-
-// x = 5 != 5;
-// console.log(x);
-
-// // > - больше
-
-// x = 5 > 4;
-// console.log(x);
-
-// // < - меньше
-
-// x = 5 < 4;
-// console.log(x);
-
-// // >= - больше либо равно 
-
-// x = 5 >= 8;
-// console.log(x);
-
-// // <= - 
-
-// x = 5 <= 8;
-// console.log(x);
-
-// // Условия 
-
-
-// // if (num > num1) {
-// //   console.log('Первое число больше второго');
-// // } else {
-// //   console.log('Второе число больше первого');
-// // }
-
-// // 
-
-
-
-
-// // && - и 
-
-// // || - или 
-// let num = 10;
-// let num1 = 5;
-// // if (num && num1) {
-// //   if (num > num1) {
-// //     console.log('Первое число больше второго');
-// //   } else if (num1 > num) {
-// //     console.log('Второе число больше первого');
-// //   } else if (num == num1) {
-// //     console.log('Числа равны');
-// //   }
-// // }else{
-// //   console.log('Error');
-// // }
-
-
-// // if (num == undefined || num1 == undefined) {
-// //   console.log('Error');
-// // } else {
-// //   if (num > num1) {
-// //     console.log('Первое число больше второго');
-// //   } else if (num1 > num) {
-// //     console.log('Второе число больше первого');
-// //   } else if (num == num1) {
-// //     console.log('Числа равны');
-// //   }
-// // }
-
-// // 
-
-
-let num = +prompt('Введите целое положительное число');
-let num1 = 0;
-
-if (num <= 0 || isNaN(num)) {
-    console.log("Введите положительное число.");
-} else {
-    for (let i = 1; i <= num; i++) {
-        num1 += i;
+for (let i = 0; i < height; i++) { 
+    for (let j = 0; j < width; j++) { 
+        if (i === 0 || i === height - 1 || j === 0 || j === width - 1) {
+            rectangle += '*';
+        } else {   
+            rectangle += ' ';
+        }
     }
-    console.log(`Сумма чисел от 1 до ${num} равна: ${num1}`);
+    rectangle += '\n'; 
 }
+
+console.log(rectangle);
